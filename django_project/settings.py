a3c2c7079c75cb6196bb22b966d327275628e607
Safidy Elliot <ravelosambatrasafidyelliot@gmail.com>
2025-11-15 13:59:39 +0300

    Corrige du settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,16 +20,16 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read.env()
+env.read_env(os.path.join(BASE_DIR, 'django_project', '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-40i9f%=1+@f8rbkt5eaw1y)&ohms_n*#l$()oicz-6w#jy102f'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["*"]
 
@@ -132,18 +132,6 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-"""""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stepicData',
-        'USER' : 'postgres',
-        'PASSWORD' : 'elliot',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
-    }
-}
-"""
 
 DATABASES = {
     'default' : dj_database_url.parse(env('DATABASE_URL'))
