1c47694c266668475ef0db28ba975afb1050a4ea
Safidy Elliot <ravelosambatrasafidyelliot@gmail.com>
2025-11-17 09:01:58 +0300

    Mise à jour pour le hachage de mot de passe

--- utilisateur/admin.py
+++ utilisateur/admin.py
@@ -17,4 +17,14 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
+    def save_model(self, request, obj, form, change):
+        # Cette condition vérifie si le mot de passe a été modifié 
+        # (c'est-à-dire s'il est un texte brut dans le formulaire)
+        if obj.pk and not obj.password.startswith('pbkdf2'):
+            obj.set_password(obj.password)
+        elif not obj.pk: # Pour la création
+             obj.set_password(obj.password)
+             
+        super().save_model(request, obj, form, change)
+
 admin.site.register(Utilisateur, CustomUserAdmin)
